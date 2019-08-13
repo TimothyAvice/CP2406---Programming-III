727c94cab09519d96eff9943b10a37c1c880cb62
TimothyAvice <timothy.avicedubuisson@my.jcu.edu.au>
2019-08-13 11:49:36 +1000

    Practical 2 Part 2 - Implemented completed Game and GameTest code along with an updated UML

--- Practicals/src/week3/Game.java
+++ Practicals/src/week3/Game.java
@@ -1,5 +1,17 @@
 package week3;
+import java.util.Random;
 
 public class Game {
-    int secret;
+    private Random random = new Random();
+    // Secret assigned random value between 1 and 10 inclusive
+    int secret = random.nextInt( 10) + 1;
+    int count;
+    int wins;
+
+    void  makeGuess(int value){
+        count += 1;
+        if (value == secret){
+            wins += 1;
+        }
+    }
 }
--- Practicals/src/week3/GameTest.java
+++ Practicals/src/week3/GameTest.java
@@ -11,5 +11,19 @@ class GameTest {
         boolean validSecret = game.secret > 0 && game.secret < 11;
         assertTrue(validSecret);
     }
+    @Test
+    void checkPlayCount(){
+        Game game = new Game();
+        assertEquals(0, game.count);
+        assertEquals(0, game.wins);
+
+        game.makeGuess(game.secret); // check for win
+        assertEquals(1, game.count);
+        assertEquals(1, game.wins);
+
+        game.makeGuess(-1); // check for a loss
+        assertEquals(2, game.count);
+        assertEquals(1, game.wins);
+    }
 
 }
