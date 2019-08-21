4557c5a3169b2b292fe8f9b772819605efb3a216
TimothyAvice <timothy.avicedubuisson@my.jcu.edu.au>
2019-08-21 10:48:37 +1000

    Practical 3 Part 7 - Added Billing and TestBilling Classes

--- Practicals/src/week3/Game.java
+++ Practicals/src/week3/Game.java
@@ -8,11 +8,15 @@ public class Game {
     int count;
     private static Random random = new Random();
     // Secret assigned random value between 1 and 10 inclusive
-    int secret = random.nextInt( 10) + 1;
+    int secret;
+
+    public Game(int min, int max){
+        secret = min + random.nextInt(max-min + 1);
+    }
 
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        Game game = new Game();
+        Game game = new Game(1,10);
         System.out.print("Enter a guess between 0 and 10: ");
         int guess = input.nextInt();
         game.makeGuess(guess);
