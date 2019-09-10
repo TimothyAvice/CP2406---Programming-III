84ae704636a5ac1be36dbff3f21f117b6c270aef
TimothyAvice <timothy.avicedubuisson@my.jcu.edu.au>
2019-09-11 09:05:19 +1000

    Practical 5 Part 9 - Completed CarCarChoice2 task

--- Practicals/src/week6/CarCareChoice2.java
+++ Practicals/src/week6/CarCareChoice2.java
@@ -3,36 +3,41 @@ package week6;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class CarCareChoice {
+public class CarCareChoice2 {
     public static void main(String[] args) {
         String input;
+        String shorthand = "vwe";
 
         Scanner scanner = new Scanner(System.in);
-        String[] services = {"Oil change", "Tire rotation", "Battery check",
-        "Brake inspection"};
+        String[] services = {"oil change", "tire rotation", "battery check",
+        "brake inspection"};
+        String[] shortservice = {"oil", "tir", "bat", "bra"};
         do {
             System.out.println("Select service. Services are :");
             for (String service: services)
                 System.out.println(service);
             System.out.print("Choice: ");
             input = scanner.nextLine();
-        }while (!Arrays.asList(services).contains(input));
+            for (int i = 0; i < shorthand.length(); i++){
+                shorthand = shorthand.replace(shorthand.charAt(i), input.charAt(i));
+            }
+        }while (!Arrays.asList(shortservice).contains(shorthand));
 
 
-        switch (input){
-            case "oil change":
+        switch (shorthand){
+            case "oil":
                 System.out.println("Choice was: " + services[0]);
                 System.out.println("Price is: $25");
                 break;
-            case "tire rotation":
+            case "tir":
                 System.out.println("Choice was: " + services[1]);
                 System.out.println("Price is: $22");
                 break;
-            case "battery check":
+            case "bat":
                 System.out.println("Choice was: " + services[2]);
                 System.out.println("Price is: $15");
                 break;
-            case "brake inspection":
+            case "bra":
                 System.out.println("Choice was: " + services[3]);
                 System.out.println("Price is: $5");
                 break;
