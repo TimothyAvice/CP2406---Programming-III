3b6afc3e99adaca7fa45f0ee9b04061d6497a9ef
TimothyAvice <timothy.avicedubuisson@my.jcu.edu.au>
2019-10-11 13:40:22 +1000

    Practical 9 Part 6 - Completed Task 2 part 8

---
+++ Practicals/src/week10/Task2/JEMail.java
@@ -0,0 +1,59 @@
+package week10.Task2;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class JEMail {
+
+    private JEMail() {
+        // Framework
+        JFrame jFrame = new JFrame("Email");
+        jFrame.setResizable(false);
+        jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        jFrame.setVisible(true);
+        jFrame.setLayout(new BoxLayout(jFrame.getContentPane(), BoxLayout.Y_AXIS));
+
+        // Text fields
+        JTextField toText = new JTextField(40);
+        JTextField subjectText = new JTextField(40);
+        JTextArea messageText = new JTextArea(30,40);
+        JScrollPane messageTextScroll = new JScrollPane(messageText);
+        // Buttons
+        JButton send = new JButton("Send Email");
+
+        JPanel to = new JPanel();
+        JPanel subject = new JPanel();
+        JPanel message = new JPanel();
+        JPanel button = new JPanel();
+
+        // Labels
+        JLabel toLabel = new JLabel("To:");
+        JLabel subjectLabel = new JLabel("Subject:");
+        JLabel messageLabel = new JLabel("Message:");
+        toLabel.setPreferredSize(new Dimension(55,30));
+        subjectLabel.setPreferredSize(new Dimension(55,30));
+        messageLabel.setPreferredSize(new Dimension(55,30));
+        messageLabel.setVerticalAlignment(JLabel.TOP);
+
+        to.add(toLabel);
+        to.add(toText);
+        subject.add(subjectLabel);
+        subject.add(subjectText);
+        message.add(messageLabel);
+        message.add(messageTextScroll);
+        button.add(send);
+
+        // Positioning components
+        jFrame.add(to);
+        jFrame.add(subject);
+        jFrame.add(message);
+        jFrame.add(button);
+        jFrame.pack();
+
+        send.addActionListener(actionEvent -> messageText.append("\nMail has been sent!"));
+    }
+
+    public static void main(String[] args) {
+        new JEMail();
+    }
+}
